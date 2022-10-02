110aeaf3328e298b6d879620fd5e73d9539372f1
alexey190900 <mte1909@mail.ru>
2022-10-02 20:39:20 +0300

    left some tests

---
+++ 03/my_list.py
@@ -0,0 +1,185 @@
+class MyList(list):
+    def __init__(self, input_lst=list()):
+        self.__lst = input_lst
+
+    @property
+    def lst(self):
+        return self.__lst
+
+
+    def __eq__(self, other):
+        sum_self = 0
+        sum_other = 0
+
+        for i in range(len(self)):
+            sum_self += self[i]
+
+        for j in range(len(other)):
+            sum_other += other[j]
+
+        if (sum_self == sum_other):
+            # print(f"...Sums equal: {sum_self} = {sum_other}")
+            return True
+
+        # print(f"...Sums arent equal: {sum_self} != {sum_other}")
+        return False
+
+
+    def __add_zeros(self, lst, num_elems):
+        list_cur_size = len(lst)
+
+        if list_cur_size < num_elems:
+            list_with_zeros = lst.copy()
+
+            for _ in range(list_cur_size, num_elems):
+                list_with_zeros.append(0)
+
+            return list_with_zeros
+
+        return lst
+
+    @classmethod
+    def __get_max_list_by_size(self, lst1, lst2):
+        lst1_size = len(lst1)
+        lst2_size = len(lst2)
+
+        if lst1_size > lst2_size:
+            return lst1
+        elif lst2_size > lst1_size:
+            return lst2
+        
+        # print(f"...Sizes equal {lst1_size=} {lst2_size=}")
+        return None
+
+    @classmethod
+    def from_my_list_to_list(cls, lst1, lst2):
+        if lst1.__class__ is cls:
+            left = lst1.lst
+        else:
+            left = lst1
+        
+        if lst2.__class__ is cls:
+            right = lst2.lst
+        else:
+            right = lst2
+        
+        return left, right
+
+    @classmethod
+    def __handle_2list(self, left, right):
+
+        upper_list = self.__class__.__get_max_list_by_size(left, right)
+
+        if upper_list is not None:
+            left_size = len(left)
+            right_size = len(right)
+            max_size = max(left_size, right_size)
+
+            if left_size < right_size:
+                lst_with_zeros = self.__add_zeros(left, max_size)
+
+                return lst_with_zeros, upper_list
+            else:
+                lst_with_zeros = self.__add_zeros(right, max_size)
+
+                return upper_list, lst_with_zeros
+    
+        return left, right
+
+
+    def __add__(self, other):
+        left, right = self.from_my_list_to_list(self, other)
+
+        left, right = self.__class__.__handle_2list(left, right)
+
+        add_lst = self.__class__([0] * len(left))
+
+        for i in range(len(left)):
+            add_lst[i] += left[i] + right[i]
+        
+        return add_lst
+
+
+    def __radd__(self, other):
+        left, right = self.from_my_list_to_list(self, other)
+
+        left_list, right_list = self.__class__.__handle_2list(left, right)
+
+        add_lst = self.__class__([0] * len(left_list))
+
+        for i in range(len(left_list)):
+            add_lst[i] += left_list[i] + right_list[i]
+        
+        return add_lst
+
+
+    def __sub__(self, other):
+        left, right = self.from_my_list_to_list(self, other)
+
+        left, right = self.__class__.__handle_2list(left, right)
+
+        add_lst = self.__class__([0] * len(left))
+
+        for i in range(len(left)):
+            add_lst[i] += left[i] - right[i]
+        
+        return add_lst
+
+
+    def __rsub__(self, other):
+        left, right = self.from_my_list_to_list(self, other)
+
+        left, right = self.__class__.__handle_2list(left, right)
+
+        add_lst = self.__class__([0] * len(left))
+
+        for i in range(len(left)):
+            add_lst[i] += left[i] - right[i]
+        
+        return add_lst
+
+
+    def __len__(self):
+        return len(self.__lst);
+
+    def __getitem__(self, key):
+        return self.__lst[key]
+
+    def __setitem__(self, key, value):
+        self.__lst[key] = value
+
+
+    def __str__(self):
+        sum_elems = 0
+        lst_size = len(self)
+
+        res_str = "["
+
+        if (len(self) != 0):
+            res_str += str(self.__lst[0])
+            sum_elems += self.__lst[0]
+
+        for i in range(1, lst_size):
+            res_str += ", " + str(self.__lst[i])
+            sum_elems += self.__lst[i]
+
+        res_str += "] " + str(sum_elems)
+
+        return res_str
+
+
+a = MyList([1, 0])
+b = MyList([0, 2, -3, 4])
+
+b_list = [0, 3, -2]
+
+# print("a = ", a)
+# print("b = ", b_list)
+
+# print((b_list + a).__class__.__name__)
+# print("a?b=", a + b_list)
+
+# print((b_list + a).__class__.__name__)
+# print("b?a=", b_list + a)
+print(dir(a))
+
---
+++ 03/tests.py
@@ -0,0 +1,106 @@
+import unittest
+import unittest.mock
+
+from my_list import MyList
+
+class TestMyList(unittest.TestCase):
+
+    def test_set_up(self):
+        self.my_list = MyList()
+
+    # __len__
+    def test_print_object(self):
+        my_list = MyList([0, -1, 2])
+        self.assertEqual(MyList.__str__(my_list), "[0, -1, 2] 1")
+
+    # __eq__
+    def test_eq_same_objects(self):
+        a = MyList([0, -1, 2])
+        b = MyList([0, -1, 2])
+
+        self.assertTrue(a == b)
+    
+    def test_eq_equal_sum(self):
+        a = MyList([0, 5, 2])
+        b = MyList([3, 0, 4])
+
+        self.assertTrue(a == b)
+
+    def test_eq_dif_objects(self):
+        a = MyList([0, -1, 2])
+        b = MyList([0, 1, 2])
+
+        self.assertFalse(a == b)
+
+    # __add_zeros
+    def test_add_zeros_same_len(self):
+        a = MyList([0, 3, 2])
+        
+        self.assertEqual(MyList._MyList__add_zeros(a, a.lst, 3), [0, 3, 2])
+
+    def test_add_zeros_dif_len(self):
+        a = MyList([0, 3, 2])
+        
+        self.assertEqual(MyList._MyList__add_zeros(a, a.lst, 5), [0, 3, 2, 0, 0])
+
+    # __get_max_list_by_size
+    def test___get_max_list_by_size_dif_len(self):
+        lst1 = [0, 1, 2, 5]
+        lst2 = [2, 3, 1]
+
+        self.assertEqual(MyList.get_max_list_by_size(lst1, lst2), [0, 1, 2, 5])
+    
+    def test___get_max_list_by_size_same_len(self):
+        lst1 = [0, 1, 2]
+        lst2 = [2, 3, 1]
+
+        self.assertIsNone(MyList._MyList__get_max_list_by_size(lst1, lst2))
+
+    # from_my_list_to_list
+    def test_from_my_list_to_list_2MyList(self):
+        a = MyList([0, -1, 2])
+        b = MyList([0, 1, 2])
+
+        self.assertEqual(MyList.from_my_list_to_list(a, b), ([0, -1, 2], [0, 1, 2]))
+
+    def test_from_my_list_to_list_1MyList_left(self):
+        a = MyList([0, -1, 2])
+        b = [0, 1, 2]
+
+        self.assertEqual(MyList.from_my_list_to_list(a, b), ([0, -1, 2], [0, 1, 2]))
+
+    def test_from_my_list_to_list_1MyList_right(self):
+        a = [0, 1, 2]
+        b = MyList([0, -1, 2])
+
+        self.assertEqual(MyList.from_my_list_to_list(a, b), ([0, 1, 2], [0, -1, 2]))
+
+    # __handle_2list
+    @unittest.mock.patch("my_list.MyList._MyList__get_max_list_by_size")
+    def test___handle_2list_left(self, mock_get_max_list):
+        a = [0, -1, 2, 3]
+        b = [0, 1]
+
+        mock_get_max_list.return_value = a
+
+        self.assertEqual(MyList._MyList__handle_2list(a, b), [0, -1, 2, 3], [0, 1, 0, 0])
+
+    @unittest.mock.patch("my_list.MyList._MyList__get_max_list_by_size")
+    def test___handle_2list_right(self, mock_get_max_list):
+        a = [0, 1]
+        b = [0, -1, 2, 3]
+
+        mock_get_max_list.return_value = a
+
+        self.assertEqual(MyList._MyList__handle_2list(a, b), [0, 1, 0, 0], [0, -1, 2, 3])
+
+    @unittest.mock.patch("my_list.MyList._MyList__get_max_list_by_size")
+    def test___handle_2list_right(self, mock_get_max_list):
+        a = [5, 1]
+        b = [0, -1]
+
+        mock_get_max_list.return_value = None
+
+        self.assertEqual(MyList._MyList__handle_2list(a, b), [5, 1], [0, -1])
+
+    
