4d194bfc565400470637a715eb91c0aa7b9890fc
alexey190900 <mte1909@mail.ru>
2022-10-10 20:06:23 +0300

    impl descriptor.py, meta.py

---
+++ 04/descriptor.py
@@ -0,0 +1,91 @@
+class Integer:
+    def __set_name__(self, owner, name):
+        self.name = name
+        self._val = f"_{name}"
+
+    def __get__(self, obj, objtype):
+        print(f"get {obj} cls={objtype}")
+
+        return getattr(obj, self._val)
+
+    def __set__(self, obj, val):
+        print(f"set {val} for {obj}")
+        if not isinstance(val, int):
+            return setattr(obj, self._val, None)
+
+        return setattr(obj, self._val, val)
+
+    def __delete__(self, obj):
+        print(f"delete from {obj}")
+
+        return delattr(obj, self._val)
+
+
+class String:
+    def __set_name__(self, owner, name):
+        self.name = name
+        self._val = f"_{name}"
+
+    def __get__(self, obj, objtype):
+        print(f"get {obj} cls={objtype}")
+
+        return getattr(obj, self._val)
+
+    def __set__(self, obj, val):
+        print(f"set {val} for {obj}")
+        if not isinstance(val, str):
+            return setattr(obj, self._val, None)
+
+        return setattr(obj, self._val, val)
+
+    def __delete__(self, obj):
+        print(f"delete from {obj}")
+
+        return delattr(obj, self._val)
+
+
+class PositiveInteger:
+    def __set_name__(self, owner, name):
+        self.name = name
+        self._val = f"_{name}"
+
+    def __get__(self, obj, objtype):
+        print(f"get {obj} cls={objtype}")
+
+        return getattr(obj, self._val)
+
+    def __set__(self, obj, val):
+        print(f"set {val} for {obj}")
+        if not isinstance(val, int) or val < 0:
+            return setattr(obj, self._val, None)
+
+        return setattr(obj, self._val, val)
+
+    def __delete__(self, obj):
+        print(f"delete from {obj}")
+
+        return delattr(obj, self._val)
+
+
+class Data:
+    num = Integer()
+    name = String()
+    price = PositiveInteger()
+
+    def __init__(self, num=None, name=None, price=None):
+        self.num = num
+        self.name = name
+        self.price = price
+
+
+if __name__ == "__main__":
+    data1 = Data()
+    data2 = Data()
+
+    data1.num = 10
+    data1.name = "Alexey"
+    data1.price = 15
+    data2.price = "jack"
+    print(data1.__dict__)
+    print(data2.__dict__)
+    
---
+++ 04/meta.py
@@ -0,0 +1,73 @@
+from types import new_class
+
+
+class CustomMeta(type):
+    def __new__(mcs, name, bases, classdict: dict, **kwargs):
+        print("Meta __new__ mcs", mcs)
+
+        new_classdict = {}
+        for key, value in classdict.items():
+            if key[0:2] != "__" and key[-1:-3] != "__":
+                key = f"custom_{key}"
+            new_classdict[key] = value
+        
+        cls = super().__new__(mcs, name, bases, new_classdict)
+        print("Meta __new__ cls(dict)", cls.__dict__)
+        return cls
+
+
+class CustomClass(metaclass=CustomMeta):
+    x = 50
+
+    def __getattribute__(self, name: str):
+        if name[0:2] != "__" and name[-1:-3] != "__" and name[0:7] != "custom_":
+            return super().__getattr__(name)
+
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name: str):
+
+        return super().__getattribute__(name)
+
+    def __setattr__(self, name: str, value):
+        if name[0:2] != "__" and name[-1:-3] != "__" and name[0:7] != "custom_":
+            name = f"custom_{name}"
+
+        return super().__setattr__(name, value)
+
+    def __init__(self, _val=99):
+        self.val = _val
+
+    def line(self):
+        return 100
+
+    def __str__(self):
+        return "Custom_by_metaclass"
+
+
+if __name__ == "__main__":
+    print("-----")
+    inst = CustomClass(30)
+    inst.hello = "hello"
+    print(inst.x)
+    
+    # print("\n", inst.__dict__)
+    # print(f"\n{CustomClass.__dict__ = }")
+    # inst.custom_x == 50
+    # inst.custom_val == 99
+    # inst.custom_line() == 100
+    # CustomClass.custom_x == 50
+    # str(inst) == "Custom_by_metaclass"
+
+
+    # print(CustomClass.__dict__)
+
+    # inst.dynamic = "added later"
+    # inst.custom_dynamic == "added later"
+    # inst.dynamic  # ошибка
+
+    # inst.x  # ошибка
+    # inst.val  # ошибка
+    # inst.line() # ошибка
+    # inst.yyy  # ошибка
+    # CustomClass.x  # ошибка
