951b31b78b870282878900968e0ec65fa251f189
alexey190900 <mte1909@mail.ru>
2022-10-23 10:36:42 +0300

    generator impl

--- 05/filter_file.py
+++ 05/filter_file.py
@@ -11,3 +11,24 @@ def filter_file(file, targets: list):
                     break
 
     return result
+
+def filter_file_gen(file, targets: list):
+    sentence = file.readline()
+    
+    while sentence != "":
+        for target in targets:
+            if target in sentence.strip().lower().split():
+                yield sentence
+                break
+        sentence = file.readline()
+
+    return None
+
+
+if __name__ == "__main__":
+    with open("file_for_filter.txt", "r", encoding='utf-8') as file_desc:
+        targets = ["роза", "яяя"]
+        result = []
+
+        for sentence in filter_file_gen(file_desc, targets):
+            result.append(sentence)
--- 05/tests.py
+++ 05/tests.py
@@ -1,28 +1,62 @@
 import unittest
 
-from filter_file import filter_file
+from filter_file import filter_file, filter_file_gen
 from lru_cache_dict import LRUCache
 
 
 class TestFilterFile(unittest.TestCase):
+    # func impl
     # targets are in file
-    def test_targets_in_file(self):
-        targets = ["роза", "яяя"]
-        self.assertEqual(
-            filter_file("file_for_filter.txt", targets),
-            [
-                '    а Роза упала на лапу Азора  \n',
-                'РОЗА\n'
-            ]
-        )
+    # def test_targets_in_file(self):
+    #     targets = ["роза", "яяя"]
+    #     self.assertEqual(
+    #         filter_file("file_for_filter.txt", targets),
+    #         [
+    #             '    а Роза упала на лапу Азора  \n',
+    #             'РОЗА\n'
+    #         ]
+    #     )
+
+    # # targets arent in file
+    # def test_targers_not_in_file(self):
+    #     targets = ["роз", "яяя"]
+    #     self.assertEqual(
+    #         filter_file("file_for_filter.txt", targets),
+    #         []
+    #     )
+
+    # generator impl
+    # targets are in file
+    def test_targets_in_file_gen(self):
+        with open("file_for_filter.txt", "r", encoding='utf-8') as file_desc:
+            targets = ["роза", "яяя"]
+            result = []
+
+            for sentence in filter_file_gen(file_desc, targets):
+                result.append(sentence)
+
+            self.assertEqual(
+                result,
+                [
+                    '    а Роза упала на лапу Азора  \n',
+                    'РОЗА\n'
+                ]
+            )
 
     # targets arent in file
-    def test_targers_not_in_file(self):
-        targets = ["роз", "яяя"]
-        self.assertEqual(
-            filter_file("file_for_filter.txt", targets),
-            []
-        )
+    def test_targers_not_in_file_gen(self):
+        with open("file_for_filter.txt", "r", encoding='utf-8') as file_desc:
+            targets = ["роз", "яяя"]
+            result = []
+
+            for sentence in filter_file_gen(file_desc, targets):
+                result.append(sentence)
+
+            self.assertEqual(
+                result,
+                [
+                ]
+            )
 
 
 class TestLRUCache(unittest.TestCase):
