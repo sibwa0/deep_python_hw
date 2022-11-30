a29cf89efec93f47098ac0ed3ccb068bb1ccdc3c
alexey190900 <mte1909@mail.ru>
2022-11-30 13:24:26 +0300

    rm code from making_hw_10 branch, commit trouble

--- 10/lru_cache_dict.py
+++ 10/lru_cache_dict.py
@@ -9,40 +9,28 @@ from utils import (
     LOGGING_CONFIG
 )
 
-logging.config.dictConfig(LOGGING_CONFIG)
-
-file_log = logging.getLogger("to_file")
-output_log = logging.getLogger("to_stdout")
-
 
 class LRUCache:
 
-    def __init__(self, limit, **kwargs):
-        self.kwargs = kwargs
+    def __init__(self, limit, logger):
+        logger.debug("Init LRUCache object")
 
-        for _, val in self.kwargs.items():
-            if val is not None:
-                val.debug("Init LRUCache object")
-
+        self.logger = logger
         self.limit = limit
         self.size = 0
         self.def_dct = defaultdict(lambda val, size: (val, size))
 
     def get(self, key):
         if key not in self.def_dct:
-            for _, val in self.kwargs.items():
-                if val is not None:
-                    val.error(
-                        f"get :: Not key( {key} )"
-                    )
+            self.logger.error(
+                f"get :: Not key( {key} )"
+            )
 
             return None
 
-        for _, val in self.kwargs.items():
-            if val is not None:
-                val.info(
-                    f"get :: Found key( {key} ) ( {self.def_dct[key][0]} )"
-                )
+        self.logger.info(
+            f"get :: Found key( {key} ) ( {self.def_dct[key][0]} )"
+        )
         return self.def_dct[key][0]
 
     def set(self, key, value):
@@ -50,46 +38,34 @@ class LRUCache:
             self.def_dct[key] = (value, self.size)
             self.size += 1
 
-            for _, val in self.kwargs.items():
-                if val is not None:
-                    val.info(
-                        f"set :: New :: key( {key} ) ( {value} ) :: "
-                        f"size( {self.size} ), limit( {self.limit} )"
-                    )
+            self.logger.info(
+                f"set :: New :: key( {key} ) ( {value} ) :: "
+                f"size( {self.size} ), limit( {self.limit} )"
+            )
 
         elif self.size == self.limit:
-            for _, val in self.kwargs.items():
-                if val is not None:
-                    val.debug(
-                        "set :: Dict is full :: "
-                        f"size( {self.size} ) = limit( {self.limit} )"
-                    )
+            self.logger.debug(
+                "set :: Dict is full :: "
+                f"size( {self.size} ) = limit( {self.limit} )"
+            )
 
             for i in self.def_dct:
-                for _, val in self.kwargs.items():
-                    if val is not None:
-                        val.debug(
-                            f"set :: Current key( {i} )"
-                            f"( {self.def_dct[i][0]} )"
-                        )
+                self.logger.debug(
+                    f"set :: Current key( {i} ) ( {self.def_dct[i][0]} )"
+                )
                 if self.def_dct[i][1] == self.size - 1:
 
                     if i == key:
-                        for _, val in self.kwargs.items():
-                            if val is not None:
-                                val.info(
-                                    f"set :: Same Key :: key( {i} ) "
-                                    f"prev( {self.def_dct[i][0]} )"
-                                    f" -> new( {value} )"
-                                )
+                        self.logger.info(
+                            f"set :: Same Key :: key( {i} ) "
+                            f"prev( {self.def_dct[i][0]} ) -> new( {value} )"
+                        )
                     else:
-                        for _, val in self.kwargs.items():
-                            if val is not None:
-                                val.info(
-                                    "set :: New Instead :: "
-                                    f"key( {i} ) ( {self.def_dct[i][0]} ) -> "
-                                    f"key( {key} ) ( {value} )"
-                                )
+                        self.logger.info(
+                            "set :: New Instead :: "
+                            f"key( {i} ) ( {self.def_dct[i][0]} ) -> "
+                            f"key( {key} ) ( {value} )"
+                        )
                         del self.def_dct[i]
 
                     self.def_dct[key] = (value, self.size - 1)
@@ -97,15 +73,13 @@ class LRUCache:
                     break
 
     def __str__(self) -> str:
-        for _, val in self.kwargs.items():
-            if val is not None:
-                val.debug(f"{str(self.def_dct)}")
+        self.logger.debug(f"{str(self.def_dct)}")
 
         return str(self.def_dct)
 
 
-def main(**kwargs):
-    cache = LRUCache(limit=2, **kwargs)
+def main(logger):
+    cache = LRUCache(limit=2, logger=logger)
 
     cache.set("k1", "val1")
     cache.set("k2", "val2")
@@ -125,7 +99,12 @@ if __name__ == "__main__":
     parser = console_input()
     console = parser.parse_args(sys.argv[1:])
 
-    if not console.s:
-        output_log = None
+    logging.config.dictConfig(LOGGING_CONFIG)
+    file = logging.getLogger("to_file")
+    output = logging.getLogger("to_stdout")
 
-    main(log1=file_log, log2=output_log)
+    if console.s:
+
+        file.addHandler(output)
+
+    main(file)
